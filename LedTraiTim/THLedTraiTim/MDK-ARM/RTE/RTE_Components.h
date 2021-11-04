046ec1ce912e4cf0c061d6ef1d4e87b40a5183d9
ThanhLe-oss <lvthanh.c3tqcap.a1@gmail.com>
2021-11-04 22:52:21 +0700

    bai tap

---
+++ LedTraiTim/THLedTraiTim/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'THLedTraiTim' 
+ * Target:  'THLedTraiTim' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
