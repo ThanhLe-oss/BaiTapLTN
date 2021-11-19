4af7aa1b30d8d41656f1790f9043622223288d38
ThanhLe-oss <lvthanh.c3tqcap.a1@gmail.com>
2021-11-19 21:16:55 +0700

    thuc hanh, bao cao

---
+++ THSTM32LCD/THSTM32ICD/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'THSTM32ICD' 
+ * Target:  'THSTM32ICD' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
